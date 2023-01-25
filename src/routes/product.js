abe7a6bf20995069f7d274a12eadd59d5c27962c
Samara Lima <samaslimag@gmail.com>
2023-01-25 09:11:50 -0300

    Controllers

--- src/routes/product.js
+++ src/routes/product.js
@@ -1,20 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
-router.post('/', (req, res, next) => {
-    res.status(201).send(req.body);
-});
+const controller = require('../controllers/product-controller');
 
-router.put('/:id', (req, res, next) => {
-    const id = req.params.id;
-    res.status(201).send({
-        id: id,
-        item: req.body
-    });
-});
-
-router.delete('/', (req, res, next) => {
-    res.status(200).send(req.body);
-});
+router.post('/', controller.post);
+router.put('/:id', controller.put);
+router.delete('/', controller.delete);
 
 module.exports = router;
