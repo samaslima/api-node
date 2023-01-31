70eafd7f4f30391e67a928bd1b8b1d008e81d93d
Samara Lima <samaslimag@gmail.com>
2023-01-31 15:03:06 -0300

    #REFACTOR Get

--- src/controllers/product-controller.js
+++ src/controllers/product-controller.js
@@ -1,6 +1,37 @@
 const mongoose = require('mongoose');
 const Product = mongoose.model('Product');
 
+
+exports.get = findAll = ('/', (req, res, next) => {
+    Product.find({ active: true }, 'title price slug')
+        .then(data => {
+            res.status(200).send(data);
+        })
+        .catch(e => {
+            res.status(400).send(e);
+        })
+})
+
+exports.getBySlug = findBySlug = ('/:slug', (req, res, next) => {
+    Product.findOne({ slug: req.params.slug, active: true }, 'title description price slug tags')
+        .then(data => {
+            res.status(200).send(data);
+        })
+        .catch(e => {
+            res.status(400).send(e);
+        })
+})
+
+exports.getById = findById = ('/:id', (req, res, next) => {
+    Product.findById(req.params.id)
+        .then(data => {
+            res.status(200).send(data);
+        })
+        .catch(e => {
+            res.status(400).send(e);
+        })
+})
+
 exports.post = create = ('/', (req, res, next) => {
     const product = new Product(req.body);
 
